2ea5296a9b62e8ec5c950161b7b2b1a3933fa022
Kiran Moktan <kirantamang184@gmail.com>
2024-12-28 11:22:33 +0000

    feat: removed admin module

--- Modules/Admin/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/admin', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
