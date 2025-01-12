e75fe9d6d9c7c5cc7833e9fc267ac1edf51c3490
Kiran Moktan <kirantamang184@gmail.com>
2025-01-12 05:28:09 +0000

    feat: setup base services

---
+++ Modules/Core/app/Traits/ResponseMessage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modules\Core\App\Traits;
+
+trait ResponseMessage
+{
+    public function successResponse(string $message = '', array|object $payload = [], int $code = 200)
+    {
+        return $this->response('success', $message, $payload, $code);
+    }
+
+    public function errorResponse(string $message = '', array|object $payload = [], int|string $code = 400)
+    {
+        $code = is_string($code) ? 500 : $code;
+        $code = (($code < 400) || ($code > 500)) ? 500 : $code;
+
+        return $this->response('error', $message, $payload, $code);
+    }
+
+    public function response(string $status, string $message, array|object $payload, int $code)
+    {
+        $response = [
+            'status' => $status,
+            'message' => $message,
+        ];
+
+        if (!empty($payload)) {
+            $response = array_merge($response, [
+                'payload' => $payload
+            ]);
+        }
+
+        return response()->json($response, $code);
+    }
+}
